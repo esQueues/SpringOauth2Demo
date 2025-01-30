7e741c601b75563c971b70cc41b9724386cd512d
Qusain Sayat <nomad1465QS@gmail.com>
2025-01-30 22:38:29 +0500

    Initial commit

---
+++ src/main/java/com/sayar/SpringOauth2Demo/HelloController.java
@@ -0,0 +1,12 @@
+package com.sayar.SpringOauth2Demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String hello() {
+        return "helloo there    !";
+    }
+}
